831b093e90d7c448b227e6dd8439bf396ff872cb
Aleksandr <menshikov.a.v@icloud.com>
2021-11-11 23:43:15 +0300

    Download books with titles

--- parser.py
+++ parser.py
@@ -1,9 +1,39 @@
+import logging
 import os
 
 import requests
+from bs4 import BeautifulSoup
 
 
-def download_books(books_amount=10):
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
+
+
+def get_book_title_and_author(book_id):
+    url = f'https://tululu.org/b{book_id}'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'lxml')
+
+    title_with_author = soup.find(class_='ow_px_td').find('h1').text
+    title, author = map(lambda x: x.strip(), title_with_author.split('::'))
+    return title, author
+
+
+def download_txt(text, book_id, filename, folder='books'):
+    filename = filename.replace(r'\\', '').replace('/', '')
+    name, extension = os.path.splitext(filename)
+    if extension != '.txt':
+        filename = f'{book_id}. {filename}.txt'
+    filepath = os.path.join(folder, filename)
+    with open(filepath, 'w') as file:
+        file.write(text)
+    return filepath
+
+
+def get_books(books_amount=10):
     for book_id in range(1, books_amount + 1):
         params = {
             'id': book_id,
@@ -11,16 +41,21 @@ def download_books(books_amount=10):
         url = 'https://tululu.org/txt.php'
         response = requests.get(url, params=params)
         response.raise_for_status()
+        try:
+            check_for_redirect(response)
+        except requests.exceptions.HTTPError:
+            logging.info(f'Книга с id = {book_id} не найдена.')
+            continue
 
-        filename = f'id{book_id}.txt'
-        with open(os.path.join('books', filename), 'w') as file:
-            file.write(response.text)
+        title, author = get_book_title_and_author(book_id)
+        download_txt(response.text, book_id, title)
 
 
 def main():
     os.makedirs('books', exist_ok=True)
-    download_books()
+    get_books()
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     main()
